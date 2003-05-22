f89c5a008f634fa7955c5381a42430c968dcc839
Administrator <irusskih@gmail.com>
2003-05-22 10:25:40 +0000

    checks current os with COMSPEC env var

--- colorer/doc/php/paths.php
+++ colorer/doc/php/paths.php
@@ -1,8 +1,11 @@
 <?
-// Unix
-//  $colorer_path = "/usr/bin/colorer"
-//  $colorer_path = "/home/groups/c/co/colorer/cgi-bin/colorer -c /home/groups/c/co/colorer/files/catalog.xml";
-
-// Win
-  $colorer_path = "d:/programs/devel/colorer/bin/colorer.exe";
+  if (getenv('COMSPEC') == ''){
+    // unix/linux
+    //$colorer_path = "/usr/bin/colorer"
+    $colorer_path = "/home/groups/c/co/colorer/cgi-bin/colorer -c /home/groups/c/co/colorer/files/catalog.xml";
+  }else{
+    //win32
+    $colorer_path = "d:/programs/devel/colorer/bin/colorer.exe";
+  };
+  echo $colorer_path;
 ?>
