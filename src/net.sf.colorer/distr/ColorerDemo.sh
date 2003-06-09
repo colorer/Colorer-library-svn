91690a2cd97e0af4607d66fd126027ec4dcaa7a6
Administrator <irusskih@gmail.com>
2003-06-09 08:53:15 +0000

    *** empty log message ***

--- src/net.sf.colorer/distr/ColorerDemo.sh
+++ src/net.sf.colorer/distr/ColorerDemo.sh
@@ -6,16 +6,16 @@
 # To use it, please provide full path to colorer/catalog.xml file
 # in %HOME%/.colorer5catalog  file.
 
-ECLIPSE_HOME=/opt/eclipse
+ECLIPSE_HOME=/usr/share/eclipse
 ECLIPSE_VERSION=2.1.0
 # Requires inclusion of additional jars:
-#ECLIPSE_TOOLKIT=gtk
-ECLIPSE_TOOLKIT=motif 
+ECLIPSE_TOOLKIT=gtk
+#ECLIPSE_TOOLKIT=motif 
 
 OS=linux
 ARCH=x86
 
-SWT_JAR=${ECLIPSE_HOME}/plugins/org.eclipse.swt.${ECLIPSE_TOOLKIT}_${ECLIPSE_VERSION}/ws/${ECLIPSE_TOOLKIT}/swt.jar
+SWT_JAR=${ECLIPSE_HOME}/plugins/org.eclipse.swt.${ECLIPSE_TOOLKIT}_${ECLIPSE_VERSION}/ws/${ECLIPSE_TOOLKIT}/swt.jar:${ECLIPSE_HOME}/plugins/org.eclipse.swt.${ECLIPSE_TOOLKIT}_${ECLIPSE_VERSION}/ws/${ECLIPSE_TOOLKIT}/swt-pi.jar
 SWT_LIB=${ECLIPSE_HOME}/plugins/org.eclipse.swt.${ECLIPSE_TOOLKIT}_${ECLIPSE_VERSION}/os/${OS}/${ARCH}
 
 env LD_LIBRARY_PATH=os/${OS}/${ARCH}:${SWT_LIB}:${LD_LIBRARY_PATH} java -classpath colorer.jar:${SWT_JAR} net.sf.colorer.swt.ColorerDemo
