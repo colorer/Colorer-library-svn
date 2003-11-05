461c885960b1d4426520da670b377426c18c4de1
russkih <irusskih@gmail.com>
2003-11-05 15:17:07 +0000

    new visual dialog for html generation

---
+++ colorer/src/net.sf.colorer/src/net/sf/colorer/viewer/HTMLGenerator.java
@@ -0,0 +1,82 @@
+/*
+ * @version $Revision$ $Date$
+ *
+ */
+package net.sf.colorer.viewer;
+
+import java.io.IOException;
+import java.io.Writer;
+
+import net.sf.colorer.ParserFactory;
+import net.sf.colorer.editor.BaseEditor;
+import net.sf.colorer.handlers.StyledRegion;
+import net.sf.colorer.impl.BaseEditorNative;
+import net.sf.colorer.impl.ReaderLineSource;
+
+/**
+ * Generates colourised HTML output from input stream,
+ * forwards output into output stream.
+ * 
+ * @author irusskih
+ */
+public class HTMLGenerator {
+
+  ReaderLineSource lineSource;
+  String hrdSchema;
+  ParserFactory pf;
+  
+  public HTMLGenerator(ParserFactory pf, ReaderLineSource input, String hrd){
+    lineSource = input;
+    hrdSchema = hrd;
+    this.pf = pf;
+  }
+  
+  
+  public void generate(Writer commonWriter, Writer escapedWriter,
+                String fileName,
+                boolean useLineNumbers,
+                boolean useHtmlSubst,
+                boolean useInfoHeader,
+                boolean useHeaderFooter) throws IOException{
+    
+    BaseEditor be = new BaseEditorNative(pf, lineSource);
+    be.setRegionCompact(true);
+    be.setRegionMapper("rgb", hrdSchema);
+    be.lineCountEvent(lineSource.getLineCount());
+    be.visibleTextEvent(0, lineSource.getLineCount());
+    be.chooseFileType(fileName);
+
+    if (useHeaderFooter){
+      commonWriter.write("<html>\n<head>\n<style></style>\n</head>\n<body style='");
+      ParsedLineWriter.writeStyle(commonWriter, (StyledRegion)be.getBackground());
+      commonWriter.write("'><pre>\n");
+    };
+              
+    if (useInfoHeader){
+      commonWriter.write("Created with Colorer-take5 Library. Type '"+be.getFileType()+"'\n\n");
+    };
+  
+    int lni = 0;
+    int lwidth = 1;
+    int lncount = lineSource.getLineCount();
+    for(lni = lncount/10; lni > 0; lni = lni/10, lwidth++);
+
+    for(int idx = 0; idx < lineSource.getLineCount(); idx++){
+      if (useLineNumbers){
+        int iwidth = 1;
+        for(lni = idx/10; lni > 0; lni = lni/10, iwidth++);
+        for(lni = iwidth; lni < lwidth; lni++) commonWriter.write(0x0020);
+        commonWriter.write(String.valueOf(idx));
+        commonWriter.write(": ");
+      }
+      ParsedLineWriter.htmlRGBWrite(commonWriter, escapedWriter, lineSource.getLine(idx), be.getLineRegions(idx));
+      commonWriter.write("\n");
+    };
+    if (useHeaderFooter){
+      commonWriter.write("</pre></body></html>\n");
+    };
+    commonWriter.close();
+  }
+  
+  
+}
