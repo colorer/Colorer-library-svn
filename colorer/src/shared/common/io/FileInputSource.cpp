8ac506d32cdef0c5b1c247cfa61388db8fb06fbe
russkih <irusskih@gmail.com>
2004-01-09 14:33:17 +0000

    file:// scheme fix for eclipse 3m6

--- colorer/src/shared/common/io/FileInputSource.cpp
+++ colorer/src/shared/common/io/FileInputSource.cpp
@@ -20,8 +20,8 @@
 FileInputSource::FileInputSource(const String *basePath, FileInputSource *base){
   if (basePath->startsWith(DString("file://"))){
     baseLocation = new SString(basePath, 7, -1);
-  }else if (basePath->startsWith(DString("file:"))){
-    baseLocation = new SString(basePath, 5, -1);
+  }else if (basePath->startsWith(DString("file:/"))){
+    baseLocation = new SString(basePath, 6, -1);
   }else{
     if (isRelative(basePath) && base != null)
       baseLocation = getAbsolutePath(base->getLocation(), basePath);
