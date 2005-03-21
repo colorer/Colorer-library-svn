058506b1d7bc77054f09599419691b6deff63f17
irusskih <irusskih@gmail.com>
2005-03-21 07:05:53 +0000

    gcc 3.4 template fix

--- colorer/src/shared/common/Hashtable.h
+++ colorer/src/shared/common/Hashtable.h
@@ -19,8 +19,8 @@ public:
 
   T get(const String *key) const{
     int hash = key->hashCode();
-    int bno = (hash&0x7FFFFFFF) % capacity;
-    for(HashEntry<T> *he = bucket[bno]; he != null; he = he->next)
+    int bno = (hash&0x7FFFFFFF) % this->capacity;
+    for(HashEntry<T> *he = this->bucket[bno]; he != null; he = he->next)
       if (he->hash == hash && *he->key == *key)
         return he->value;
     return null;
@@ -30,7 +30,7 @@ public:
       Returns first element value in a sequence, or null, if hashtable is empty.
   */
   T enumerate() const{
-    T * retval = enumerate_int();
+    T * retval = this->enumerate_int();
     if (retval == null) return null;
     return *retval;
   };
@@ -39,7 +39,7 @@ public:
       is thrown. If end of hash is reached, exception is thrown.
   */
   T next() const{
-    T *retval = next_int();
+    T *retval = this->next_int();
     if (retval == null) return null;
     return *retval;
   };
@@ -61,8 +61,8 @@ public:
 
   const T *get(const String *key) const{
     int hash = key->hashCode();
-    int bno = (hash&0x7FFFFFFF) % capacity;
-    for(HashEntry<T> *he = bucket[bno]; he != null; he = he->next)
+    int bno = (hash&0x7FFFFFFF) % this->capacity;
+    for(HashEntry<T> *he = this->bucket[bno]; he != null; he = he->next)
       if (he->hash == hash && *he->key == *key)
         return &he->value;
     return null;
@@ -72,14 +72,14 @@ public:
       Returns first element value in a sequence, or null, if hashtable is empty
   */
   T *enumerate() const{
-    return enumerate_int();
+    return this->enumerate_int();
   };
   /** Returns the next value object with current enumeration procedure.
       If hashtable state is changed, and next() call occurs, exception
       is thrown. If end of hash is reached, exception is thrown.
   */
   T *next() const{
-    return next_int();
+    return this->next_int();
   };
 };
 
