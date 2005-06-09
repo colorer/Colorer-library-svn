126f7921edd084674af5ad2510e8e46c21eaae74
irusskih <irusskih@gmail.com>
2005-06-09 11:20:49 +0000

    *** empty log message ***

--- src/shared/colorer/viewer/ConsoleTools.cpp
+++ src/shared/colorer/viewer/ConsoleTools.cpp
@@ -398,6 +398,9 @@ void ConsoleTools::genOutput(bool useTokens){
   };
 }
 
+void ConsoleTools::genTokenOutput(){
+  genOutput(true);
+}
 
 /* ***** BEGIN LICENSE BLOCK *****
  * Version: MPL 1.1/GPL 2.0/LGPL 2.1
