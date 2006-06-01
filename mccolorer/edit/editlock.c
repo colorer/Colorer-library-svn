9e2a33a4d24502cbf3bc8c9ea2ef077e503ffcb1
lomecb <irusskih@gmail.com>
2006-06-01 07:25:11 +0000

    MC vendor release 4.6.1

---
+++ mccolorer/edit/editlock.c
@@ -0,0 +1,215 @@
+/* editor file locking.
+
+   Copyright (C) 2003 the Free Software Foundation
+
+   Authors: 2003 Adam Byrtek
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+   02110-1301, USA.
+
+*/
+
+#include <config.h>
+#include <signal.h>		/* kill() */
+#include "edit.h"
+#include "editlock.h"
+
+#include "../src/wtools.h"	/* edit_query_dialog () */
+
+#define BUF_SIZE 255
+#define PID_BUF_SIZE 10
+
+struct lock_s {
+    char *who;
+    pid_t pid;
+};
+
+/* Locking scheme used in mcedit is based on a documentation found
+   in JED editor sources. Abstract from lock.c file (by John E. Davis):
+
+   The basic idea here is quite simple.  Whenever a buffer is attached to
+   a file, and that buffer is modified, then attempt to lock the
+   file. Moreover, before writing to a file for any reason, lock the
+   file. The lock is really a protocol respected and not a real lock.
+   The protocol is this: If in the directory of the file is a
+   symbolic link with name ".#FILE", the FILE is considered to be locked
+   by the process specified by the link.
+*/
+
+/* Build user@host.domain.pid string (need to be freed) */
+static char *
+lock_build_name (const char *fname)
+{
+    char host[BUF_SIZE];
+    const char *user;
+
+    if (!
+	((user = getpwuid (getuid ())->pw_name) || (user = getenv ("USER"))
+	 || (user = getenv ("USERNAME")) || (user = getenv ("LOGNAME"))))
+	user = "";
+
+    /* TODO: Use FQDN, no clean interface, so requires lot of code */
+    if (gethostname (host, BUF_SIZE - 1) == -1)
+	*host = '\0';
+
+    return g_strdup_printf ("%s@%s.%d", user, host, (int) getpid ());
+}
+
+/* Extract pid from user@host.domain.pid string */
+static struct lock_s *
+lock_extract_info (const char *str)
+{
+    int i;
+    const char *p, *s;
+    static char pid[PID_BUF_SIZE], who[BUF_SIZE];
+    static struct lock_s lock;
+
+    for (p = str + strlen (str) - 1; p >= str; p--)
+	if (*p == '.')
+	    break;
+
+    /* Everything before last '.' is user@host */
+    i = 0;
+    for (s = str; s < p && i < BUF_SIZE; s++)
+	who[i++] = *s;
+    who[i] = '\0';
+
+    /* Treat text between '.' and ':' or '\0' as pid */
+    i = 0;
+    for (p = p + 1;
+	 p < str + strlen (str) && *p != ':' && i < PID_BUF_SIZE; p++)
+	pid[i++] = *p;
+    pid[i] = '\0';
+
+    lock.pid = (pid_t) atol (pid);
+    lock.who = who;
+    return &lock;
+}
+
+/* Extract user@host.domain.pid from lock file (static string)  */
+static char *
+lock_get_info (const char *lockfname)
+{
+    int cnt;
+    static char buf[BUF_SIZE];
+
+    if ((cnt = readlink (lockfname, buf, BUF_SIZE - 1)) == -1 || !buf
+	|| !*buf)
+	return NULL;
+    buf[cnt] = '\0';
+    return buf;
+}
+
+
+/* Tries to raise file lock
+   Returns 1 on success,  0 on failure, -1 if abort 
+   Warning: Might do screen refresh and lose edit->force */
+int
+edit_lock_file (const char *fname)
+{
+    char *lockfname, *newlock, *msg, *lock;
+    struct stat statbuf;
+    struct lock_s *lockinfo;
+
+    /* Just to be sure (and don't lock new file) */
+    if (!fname || !*fname)
+	return 0;
+
+    /* Locking on VFS is not supported */
+    if (!vfs_file_is_local (fname))
+	return 0;
+
+    /* Check if already locked */
+    lockfname = g_strconcat (".#", fname, (char *) NULL);
+    if (lstat (lockfname, &statbuf) == 0) {
+	lock = lock_get_info (lockfname);
+	if (!lock) {
+	    g_free (lockfname);
+	    return 0;
+	}
+	lockinfo = lock_extract_info (lock);
+
+	/* Check if locking process alive, ask user if required */
+	if (!lockinfo->pid
+	    || !(kill (lockinfo->pid, 0) == -1 && errno == ESRCH)) {
+	    msg =
+		g_strdup_printf (_
+				 ("File \"%s\" is already being edited\n"
+				  "User: %s\nProcess ID: %d"), fname,
+				 lockinfo->who, (int) lockinfo->pid);
+	    /* TODO: Implement "Abort" - needs to rewind undo stack */
+	    switch (edit_query_dialog2
+		    (_("File locked"), msg, _("&Grab lock"),
+		     _("&Ignore lock"))) {
+	    case 0:
+		break;
+	    case 1:
+	    case -1:
+		g_free (lockfname);
+		g_free (msg);
+		return 0;
+	    }
+	    g_free (msg);
+	}
+	unlink (lockfname);
+    }
+
+    /* Create lock symlink */
+    newlock = lock_build_name (fname);
+    if (symlink (newlock, lockfname) == -1) {
+	g_free (lockfname);
+	g_free (newlock);
+	return 0;
+    }
+
+    g_free (lockfname);
+    g_free (newlock);
+    return 1;
+}
+
+/* Lowers file lock if possible 
+   Always returns 0 to make 'lock = edit_unlock_file (f)' possible */
+int
+edit_unlock_file (const char *fname)
+{
+    char *lockfname, *lock;
+    struct stat statbuf;
+
+    /* Just to be sure */
+    if (!fname || !*fname)
+	return 0;
+
+    lockfname = g_strconcat (".#", fname, (char *) NULL);
+
+    /* Check if lock exists */
+    if (lstat (lockfname, &statbuf) == -1) {
+	g_free (lockfname);
+	return 0;
+    }
+
+    lock = lock_get_info (lockfname);
+    if (lock) {
+	/* Don't touch if lock is not ours */
+	if (lock_extract_info (lock)->pid != getpid ()) {
+	    g_free (lockfname);
+	    return 0;
+	}
+    }
+
+    /* Remove lock */
+    unlink (lockfname);
+    g_free (lockfname);
+    return 0;
+}
---
+++ mccolorer/edit/editwidget.c
@@ -0,0 +1,364 @@
+/* editor initialisation and callback handler.
+
+   Copyright (C) 1996, 1997 the Free Software Foundation
+
+   Authors: 1996, 1997 Paul Sheer
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+   02110-1301, USA.
+ */
+
+#include <config.h>
+#include "edit.h"
+#include "edit-widget.h"
+
+#include "../src/tty.h"		/* LINES */
+#include "../src/widget.h"	/* redraw_labels() */
+#include "../src/menu.h"	/* menubar_new() */
+#include "../src/key.h"		/* is_idle() */
+
+WEdit *wedit;
+struct WMenu *edit_menubar;
+
+int column_highlighting = 0;
+
+static cb_ret_t edit_callback (WEdit *edit, widget_msg_t msg, int parm);
+
+static int
+edit_event (WEdit * edit, Gpm_Event * event, int *result)
+{
+    *result = MOU_NORMAL;
+    edit_update_curs_row (edit);
+    edit_update_curs_col (edit);
+
+    /* Unknown event type */
+    if (!(event->type & (GPM_DOWN | GPM_DRAG | GPM_UP)))
+	return 0;
+
+    /* Wheel events */
+    if ((event->buttons & GPM_B_UP) && (event->type & GPM_DOWN)) {
+	edit_move_up (edit, 2, 1);
+	goto update;
+    }
+    if ((event->buttons & GPM_B_DOWN) && (event->type & GPM_DOWN)) {
+	edit_move_down (edit, 2, 1);
+	goto update;
+    }
+
+    /* Outside editor window */
+    if (event->y <= 1 || event->x <= 0
+	|| event->x > edit->num_widget_columns
+	|| event->y > edit->num_widget_lines + 1)
+	return 0;
+
+    /* A lone up mustn't do anything */
+    if (edit->mark2 != -1 && event->type & (GPM_UP | GPM_DRAG))
+	return 1;
+
+    if (event->type & (GPM_DOWN | GPM_UP))
+	edit_push_key_press (edit);
+
+    edit_cursor_move (edit, edit_bol (edit, edit->curs1) - edit->curs1);
+
+    if (--event->y > (edit->curs_row + 1))
+	edit_cursor_move (edit,
+			  edit_move_forward (edit, edit->curs1,
+					     event->y - (edit->curs_row +
+							 1), 0)
+			  - edit->curs1);
+
+    if (event->y < (edit->curs_row + 1))
+	edit_cursor_move (edit,
+			  edit_move_backward (edit, edit->curs1,
+					      (edit->curs_row + 1) -
+					      event->y) - edit->curs1);
+
+    edit_cursor_move (edit,
+		      (int) edit_move_forward3 (edit, edit->curs1,
+						event->x -
+						edit->start_col - 1,
+						0) - edit->curs1);
+
+    edit->prev_col = edit_get_col (edit);
+
+    if (event->type & GPM_DOWN) {
+	edit_mark_cmd (edit, 1);	/* reset */
+	edit->highlight = 0;
+    }
+
+    if (!(event->type & GPM_DRAG))
+	edit_mark_cmd (edit, 0);
+
+  update:
+    edit->force |= REDRAW_COMPLETELY;
+    edit_update_curs_row (edit);
+    edit_update_curs_col (edit);
+    edit_update_screen (edit);
+
+    return 1;
+}
+
+
+static int
+edit_mouse_event (Gpm_Event *event, void *x)
+{
+    int result;
+    if (edit_event ((WEdit *) x, event, &result))
+	return result;
+    else
+	return (*edit_menubar->widget.mouse) (event, edit_menubar);
+}
+
+static void
+edit_adjust_size (Dlg_head *h)
+{
+    WEdit *edit;
+    WButtonBar *edit_bar;
+
+    edit = (WEdit *) find_widget_type (h, (callback_fn) edit_callback);
+    edit_bar = find_buttonbar (h);
+
+    widget_set_size (&edit->widget, 0, 0, LINES - 1, COLS);
+    widget_set_size (&edit_bar->widget, LINES - 1, 0, 1, COLS);
+    widget_set_size (&edit_menubar->widget, 0, 0, 1, COLS);
+
+#ifdef RESIZABLE_MENUBAR
+    menubar_arrange (edit_menubar);
+#endif
+}
+
+/* Callback for the edit dialog */
+static cb_ret_t
+edit_dialog_callback (Dlg_head *h, dlg_msg_t msg, int parm)
+{
+    WEdit *edit;
+
+    switch (msg) {
+    case DLG_RESIZE:
+	edit_adjust_size (h);
+	return MSG_HANDLED;
+
+    case DLG_VALIDATE:
+	edit = (WEdit *) find_widget_type (h, (callback_fn) edit_callback);
+	if (!edit_ok_to_exit (edit)) {
+	    h->running = 1;
+	}
+	return MSG_HANDLED;
+
+    default:
+	return default_dlg_callback (h, msg, parm);
+    }
+}
+
+int
+edit_file (const char *_file, int line)
+{
+    static int made_directory = 0;
+    Dlg_head *edit_dlg;
+    WButtonBar *edit_bar;
+
+    if (option_backup_ext_int != -1) {
+	option_backup_ext = g_malloc (sizeof (int) + 1);
+	option_backup_ext[sizeof (int)] = '\0';
+	memcpy (option_backup_ext, (char *) &option_backup_ext_int,
+		sizeof (int));
+    }
+    if (!made_directory) {
+	mkdir (catstrs (home_dir, EDIT_DIR, (char *) NULL), 0700);
+	made_directory = 1;
+    }
+
+    if (!(wedit = edit_init (NULL, LINES - 2, COLS, _file, line))) {
+	return 0;
+    }
+
+    /* Create a new dialog and add it widgets to it */
+    edit_dlg =
+	create_dlg (0, 0, LINES, COLS, NULL, edit_dialog_callback,
+		    "[Internal File Editor]", NULL, DLG_WANT_TAB);
+
+    init_widget (&(wedit->widget), 0, 0, LINES - 1, COLS,
+		 (callback_fn) edit_callback,
+		 (mouse_h) edit_mouse_event);
+
+    widget_want_cursor (wedit->widget, 1);
+
+    edit_bar = buttonbar_new (1);
+
+    switch (edit_key_emulation) {
+    case EDIT_KEY_EMULATION_NORMAL:
+	edit_init_menu_normal ();	/* editmenu.c */
+	break;
+    case EDIT_KEY_EMULATION_EMACS:
+	edit_init_menu_emacs ();	/* editmenu.c */
+	break;
+    }
+    edit_menubar = menubar_new (0, 0, COLS, EditMenuBar, N_menus);
+
+    add_widget (edit_dlg, edit_bar);
+    add_widget (edit_dlg, wedit);
+    add_widget (edit_dlg, edit_menubar);
+
+    run_dlg (edit_dlg);
+
+    edit_done_menu ();		/* editmenu.c */
+
+    destroy_dlg (edit_dlg);
+
+    return 1;
+}
+
+static void edit_my_define (Dlg_head * h, int idx, const char *text,
+			    void (*fn) (WEdit *), WEdit * edit)
+{
+    define_label_data (h, idx, text, (buttonbarfn) fn, edit);
+}
+
+
+static void cmd_F1 (WEdit * edit)
+{
+    send_message ((Widget *) edit, WIDGET_KEY, KEY_F (1));
+}
+
+static void cmd_F2 (WEdit * edit)
+{
+    send_message ((Widget *) edit, WIDGET_KEY, KEY_F (2));
+}
+
+static void cmd_F3 (WEdit * edit)
+{
+    send_message ((Widget *) edit, WIDGET_KEY, KEY_F (3));
+}
+
+static void cmd_F4 (WEdit * edit)
+{
+    send_message ((Widget *) edit, WIDGET_KEY, KEY_F (4));
+}
+
+static void cmd_F5 (WEdit * edit)
+{
+    send_message ((Widget *) edit, WIDGET_KEY, KEY_F (5));
+}
+
+static void cmd_F6 (WEdit * edit)
+{
+    send_message ((Widget *) edit, WIDGET_KEY, KEY_F (6));
+}
+
+static void cmd_F7 (WEdit * edit)
+{
+    send_message ((Widget *) edit, WIDGET_KEY, KEY_F (7));
+}
+
+static void cmd_F8 (WEdit * edit)
+{
+    send_message ((Widget *) edit, WIDGET_KEY, KEY_F (8));
+}
+
+#if 0
+static void cmd_F9 (WEdit * edit)
+{
+    send_message ((Widget *) edit, WIDGET_KEY, KEY_F (9));
+}
+#endif
+
+static void cmd_F10 (WEdit * edit)
+{
+    send_message ((Widget *) edit, WIDGET_KEY, KEY_F (10));
+}
+
+static void
+edit_labels (WEdit *edit)
+{
+    Dlg_head *h = edit->widget.parent;
+
+    edit_my_define (h, 1, _("Help"), cmd_F1, edit);
+    edit_my_define (h, 2, _("Save"), cmd_F2, edit);
+    edit_my_define (h, 3, _("Mark"), cmd_F3, edit);
+    edit_my_define (h, 4, _("Replac"), cmd_F4, edit);
+    edit_my_define (h, 5, _("Copy"), cmd_F5, edit);
+    edit_my_define (h, 6, _("Move"), cmd_F6, edit);
+    edit_my_define (h, 7, _("Search"), cmd_F7, edit);
+    edit_my_define (h, 8, _("Delete"), cmd_F8, edit);
+    edit_my_define (h, 9, _("PullDn"), edit_menu_cmd, edit);
+    edit_my_define (h, 10, _("Quit"), cmd_F10, edit);
+
+    redraw_labels (h);
+}
+
+void edit_update_screen (WEdit * e)
+{
+    edit_scroll_screen_over_cursor (e);
+
+    edit_update_curs_col (e);
+    edit_status (e);
+
+/* pop all events for this window for internal handling */
+
+    if (!is_idle ()) {
+	e->force |= REDRAW_PAGE;
+	return;
+    }
+    if (e->force & REDRAW_COMPLETELY)
+	e->force |= REDRAW_PAGE;
+    edit_render_keypress (e);
+}
+
+static cb_ret_t
+edit_callback (WEdit *e, widget_msg_t msg, int parm)
+{
+    switch (msg) {
+    case WIDGET_INIT:
+	e->force |= REDRAW_COMPLETELY;
+	edit_labels (e);
+	return MSG_HANDLED;
+
+    case WIDGET_DRAW:
+	e->force |= REDRAW_COMPLETELY;
+	e->num_widget_lines = LINES - 2;
+	e->num_widget_columns = COLS;
+	/* fallthrough */
+
+    case WIDGET_FOCUS:
+	edit_update_screen (e);
+	return MSG_HANDLED;
+
+    case WIDGET_KEY:
+	{
+	    int cmd, ch;
+
+	    /* first check alt-f, alt-e, alt-s, etc for drop menus */
+	    if (edit_drop_hotkey_menu (e, parm))
+		return MSG_HANDLED;
+	    if (!edit_translate_key (e, parm, &cmd, &ch))
+		return MSG_NOT_HANDLED;
+	    edit_execute_key_command (e, cmd, ch);
+	    edit_update_screen (e);
+	}
+	return MSG_HANDLED;
+
+    case WIDGET_CURSOR:
+	widget_move (&e->widget, e->curs_row + EDIT_TEXT_VERTICAL_OFFSET,
+		     e->curs_col + e->start_col);
+	return MSG_HANDLED;
+
+    case WIDGET_DESTROY:
+	edit_clean (e);
+	return MSG_HANDLED;
+
+    default:
+	return default_proc (msg, parm);
+    }
+}
