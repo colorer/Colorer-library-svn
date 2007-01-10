28399782172fd161d46df93c270c24c066ccdb87
lomecb <irusskih@gmail.com>
2007-01-10 11:48:14 +0000

    base editor performance fix streamwriter checks for file handle now

--- src/shared/common/io/StreamWriter.cpp
+++ src/shared/common/io/StreamWriter.cpp
@@ -3,28 +3,34 @@
 #include<common/io/StreamWriter.h>
 
 StreamWriter::StreamWriter(){};
+
 void StreamWriter::init(FILE *fstream, int encoding, bool useBOM){
+  
+  if (fstream == 0) throw Exception(DString("Invalid stream"));
   file = fstream;
   if (encoding == -1) encoding = Encodings::getDefaultEncodingIndex();
   encodingIndex = encoding;
   this->useBOM = useBOM;
   writeBOM();
-};
+}
+
 void StreamWriter::writeBOM(){
   if (useBOM && Encodings::isMultibyteEncoding(encodingIndex)) write(0xFEFF);
-};
+}
 
 StreamWriter::StreamWriter(FILE *fstream, int encoding = -1, bool useBOM = true){
   init(fstream, encoding, useBOM);
-};
+}
+
 StreamWriter::~StreamWriter(){
-};
+}
+
 void StreamWriter::write(wchar c){
   byte buf[8];
   int bufLen = Encodings::toBytes(encodingIndex, c, buf);
   for(int pos = 0; pos < bufLen; pos++)
     putc(buf[pos], file);
-};
+}
 
 /* ***** BEGIN LICENSE BLOCK *****
  * Version: MPL 1.1/GPL 2.0/LGPL 2.1
