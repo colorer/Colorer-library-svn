13c221688cdf5b83da6fabed114d651696a370c5
lomecb <irusskih@gmail.com>
2007-01-16 08:10:04 +0000

    Created folder remotely

--- src/buildprop.bat
+++
@@ -1,3 +0,0 @@
-native2ascii.exe  .\net\sf\colorer\HRCMessages_ru.prop .\net\sf\colorer\HRCMessages_ru.properties
-native2ascii.exe  .\net\sf\colorer\eclipse\Messages_ru.prop .\net\sf\colorer\eclipse\Messages_ru.properties
-native2ascii.exe  .\net\sf\colorer\swt\dialog\Locale_ru.prop .\net\sf\colorer\swt\dialog\Locale_ru.properties
