58e7a295419437090ef9e82f19c7fff8e749dc9e
lomecb <irusskih@gmail.com>
2010-02-06 14:27:12 +0000

    improved haml outline, haml injections. ruby minors

--- bin/rubygen/rubygen.rb
+++ bin/rubygen/rubygen.rb
@@ -2,6 +2,14 @@
 @m_scanned = {}
 @v_scanned = {}
 
+module Kernel
+  def autoload sym, path
+    warn sym
+    warn path
+    require path
+  end
+end
+
 def gen(klass)
   @c_scanned[klass.to_s] = 1
 
@@ -10,11 +18,24 @@ def gen(klass)
     @m_scanned[m] = 1
     # klass.to_s + "." + 
   end
+  klass.protected_instance_methods(false).each do |m|
+    next if m.length < 3
+    @m_scanned[m] = 1
+  end
 
   klass.public_methods(false).each do |m|
     next if m.length < 3
     @m_scanned[m] = 1
   end
+
+  klass.protected_methods(false).each do |m|
+    next if m.length < 3
+    @m_scanned[m] = 1
+  end
+
+  klass.included_modules.each do |m|
+    gen(m)
+  end   
 
   #klass.public_methods.each do |m|
   #  @m_scanned[m] = 1
@@ -22,15 +43,14 @@ def gen(klass)
 
 end
 
-
-#Kernel.public_methods.each{|m| push m}
-
-
 ObjectSpace.each_object(Class) do |klass|
-
   gen klass
-
 end
+
+ObjectSpace.each_object(Module) do |klass|
+  gen klass
+end
+
 
 @c_scanned.each_key do |c|
   puts "        <word name='#{c}'/>"
@@ -39,7 +59,7 @@ end
 2.times { puts }
 
 @m_scanned.each_key do |m|
-  puts "        <symb name='.#{m}'/>"
+  puts "        <word name='#{m}'/>"
 end
 
 2.times { puts }
