3a57c90b3d55b1bc195f98904753eb6397c04ac7
formirror <formirror@gmail.com>
2010-02-18 19:29:03 +0000

    Merging r1386 through r1399 from Efremov-hrc

--- schemes/build.cmd
+++ schemes/build.cmd
@@ -3,5 +3,5 @@
 rem path to java libs
 set CLR_JL=javalib
 
-set CLASSPATH=%CLR_JL%\commons-net-ftp-2.0.jar;%CLR_JL%\resolver.jar;%CLR_JL%\saxon9.jar;%CLASSPATH%
+set CLASSPATH=%CLR_JL%\commons-net-ftp-2.0.jar;%CLR_JL%\commons-net-2.0.jar;%CLR_JL%\resolver.jar;%CLR_JL%\saxon9.jar;%CLASSPATH%
 ant %*
