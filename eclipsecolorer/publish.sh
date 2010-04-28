a06cf2393f9be61aae7e370988c8e3bf32fb15e3
lomecb <irusskih@gmail.com>
2010-04-28 21:57:56 +0000

    finalizing eclipsecolorer setup, compiled libs are under svn

---
+++ eclipsecolorer/publish.sh
@@ -0,0 +1,5 @@
+scp EclipseColorer*/update/site.xml lomecb,colorer@shell.sf.net:/home/groups/c/co/colorer/htdocs/eclipsecolorer/site.xml
+scp EclipseColorer*/update/features/*.jar lomecb,colorer@shell.sf.net:/home/groups/c/co/colorer/htdocs/eclipsecolorer/features/
+scp EclipseColorer*/update/plugins/*.jar lomecb,colorer@shell.sf.net:/home/groups/c/co/colorer/htdocs/eclipsecolorer/plugins/
+
+
