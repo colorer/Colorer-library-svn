4efe8a5baca02130a4bd45d64438fb30c97a7787
lomecb <irusskih@gmail.com>
2010-04-29 19:00:55 +0000

    fixing update issues

--- eclipsecolorer/publish.sh
+++ eclipsecolorer/publish.sh
@@ -1,5 +1,5 @@
-scp EclipseColorer*/update/site.xml lomecb,colorer@shell.sf.net:/home/groups/c/co/colorer/htdocs/eclipsecolorer/site.xml
-scp EclipseColorer*/update/features/*.jar lomecb,colorer@shell.sf.net:/home/groups/c/co/colorer/htdocs/eclipsecolorer/features/
-scp EclipseColorer*/update/plugins/*.jar lomecb,colorer@shell.sf.net:/home/groups/c/co/colorer/htdocs/eclipsecolorer/plugins/
+scp EclipseColorer*/update/site.xml lomecb,colorer@web.sf.net:/home/groups/c/co/colorer/htdocs/eclipsecolorer/site.xml
+scp EclipseColorer*/update/features/*.jar lomecb,colorer@web.sf.net:/home/groups/c/co/colorer/htdocs/eclipsecolorer/features/
+scp EclipseColorer*/update/plugins/*.jar lomecb,colorer@web.sf.net:/home/groups/c/co/colorer/htdocs/eclipsecolorer/plugins/
 
 
