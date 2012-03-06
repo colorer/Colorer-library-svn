6ff7ae82d3bdc1ea4c75a6ea7d3a7dfc9256525a
ctapmex <ctapmex@gmail.com>
2012-03-06 05:17:32 +0000

    environment variables in colorer.exe

--- colorer/src/shared/colorer/viewer/ConsoleTools.cpp
+++ colorer/src/shared/colorer/viewer/ConsoleTools.cpp
@@ -84,7 +84,16 @@ void ConsoleTools::setOutputEncoding(const String &str) {
 
 void ConsoleTools::setCatalogPath(const String &str) {
   delete catalogPath;
+#if defined _WIN32
+   // replace the environment variables to their values
+  size_t i=ExpandEnvironmentStringsW(str.getWChars(),NULL,0);
+  wchar_t *temp = new wchar_t[i];
+  ExpandEnvironmentStringsW(str.getWChars(),temp,static_cast<DWORD>(i));
+  catalogPath = new SString(temp);
+  delete[] temp;
+#else
   catalogPath = new SString(str);
+#endif
 }
 
 void ConsoleTools::setHRDName(const String &str) {
