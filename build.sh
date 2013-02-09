a02b2b23c6f37892f41023c8ce0044638c00e8c6
lazyeugene <lazyeugene@gmail.com>
2013-02-09 13:18:18 +0000

    * trunk merged * jscript scheme fix

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 
 # rem path to java libs
-export CLR_JL=javalib
+export CLR_JL=./javalib
 
 export CLASSPATH=$CLR_JL/commons-net-ftp-2.0.jar:$CLR_JL/commons-net-2.0.jar:$CLR_JL/resolver.jar:$CLR_JL/saxon9.jar:$CLASSPATH
 
